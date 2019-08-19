d9f361f07191f70726f5d2feae12255652aacce0
Sebastian Bergmann <sb@sebastian-bergmann.de>
2019-08-19 15:48:04 +0200

    Detect changes in https://github.com/bebbo/gcc repository

--- bump.php
+++ bump.php
@@ -1,5 +1,5 @@
 #!/usr/bin/env php
-<?php
+<?php declare(strict_types=1);
 $tmp = file_get_contents(__DIR__ . '/Dockerfile');
 $old = substr($tmp, strpos($tmp, 'git checkout -qf ') + strlen('git checkout -qf '), 40);
 $new = trim(`git ls-remote https://github.com/bebbo/amiga-gcc.git | grep HEAD | awk '{ print $1}'`);
@@ -8,4 +8,19 @@ file_put_contents(
     __DIR__ . '/Dockerfile',
     str_replace($old, $new, $tmp)
 );
+
+$old = '';
+
+if (file_exists(__DIR__ . '/gcc-latest-commit')) {
+    $old = trim(file_get_contents(__DIR__ . '/gcc-latest-commit'));
+}
+
+$new = trim(`git ls-remote https://github.com/bebbo/gcc.git | grep HEAD | awk '{ print $1}'`);
+
+if ($old !== $new) {
+    print 'https://github.com/bebbo/gcc.git has new commits' . PHP_EOL;
+
+    file_put_contents(__DIR__ . '/gcc-latest-commit', $new);
+}
+
 
